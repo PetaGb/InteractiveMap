8d959afa7ba70dd171f79e2adbd0e12f99e895aa
PetaGb <peter.gelec@gmail.com>
2023-02-05 19:08:47 +0100

    creating flask object

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-import pandas as pd
+from flask import Flask, render_template
 
-import plotly.express as px
+from map import map_fig
 
-df = pd.read_csv("dataset.csv")
+app = Flask(__name__)
 
+@app.route("/")
 
-Western_Europe = df[df['Regional indicator'] == 'Western Europe']
-Central_and_Eastern_Europe = df[df['Regional indicator'] == 'Central and Eastern Europe']
-Europe = pd.concat([Western_Europe, Central_and_Eastern_Europe])
+def index():
+    return render_template("map_template.html", fig=map_fig())
 
-
-cols = ['Country name', 'Ladder score', 'Logged GDP per capita', 'Social support',
-        'Healthy life expectancy', 'Freedom to make life choices', 'Generosity', 'Perceptions of corruption']
-
-fig = px.choropleth(Europe,
-                    scope="europe",
-                    locations="Country name",
-                    locationmode='country names',
-                    color='Ladder score',
-                    hover_name='Ladder score',
-                    hover_data=cols,
-                    color_continuous_scale=px.colors.sequential.Blues,
-                    )
-
-fig.update_layout(height=800, width=1600, margin={"r": 1, "t": 0, "l": 0, "b": 0})
-fig.show()
+if __name__ == "__main__":
+    app.run(debug=True)
